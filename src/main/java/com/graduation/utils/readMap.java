b455d3e84104e7318527e993e8fa60ee85e2452f
AdrianT <74223068+AdrTowers@users.noreply.github.com>
2021-09-09 10:03:36 -0700

    Added school year to the status bar so it displays your current grade. AB#423

--- src/main/java/com/graduation/utils/readMap.java
+++ src/main/java/com/graduation/utils/readMap.java
@@ -10,19 +10,18 @@ import java.util.HashMap;
 
 public class readMap {
 
-
     private static String map;
 
-
-    public static String convertedMap(){
+    public static String convertedMap() {
         String newMap = null;
         HashMap<String, String> mapCode = new HashMap<>();
         mapCode.put("Gym", "  ");
         mapCode.put("Geography", "  ");
         mapCode.put("Player", GameClient.getPlayer().getName());
         mapCode.put("Hallway", "  ");
-        mapCode.put("Credit", String.valueOf(GameClient.getPlayer().getCredit()));
+        mapCode.put("Grade", String.valueOf(Player.getGrade()));
         mapCode.put("Maths", "  ");
+        mapCode.put("Credit", String.valueOf(GameClient.getPlayer().getCredit()));
         mapCode.put("Cafeteria", "  ");
         mapCode.put("History", "  ");
         mapCode.put("Computers", "  ");
@@ -32,38 +31,34 @@ public class readMap {
             mapCode.replace(GameClient.getPlayer().getLocation(), "**");
 
             newMap = String.format(map, mapCode.get("Gym"), mapCode.get("Geography"),
-                    mapCode.get("Player"), mapCode.get("Hallway"), mapCode.get("Credit"),
+                    mapCode.get("Player") ,mapCode.get("Hallway"), mapCode.get("Grade"),
                     mapCode.get("Maths"), mapCode.get("Cafeteria"), mapCode.get("History"),
-                    mapCode.get("Computers"));
+                    mapCode.get("Credit"), mapCode.get("Computers"));
 
-        }
-        else if (Player.getGrade() == Grade.SOPHOMORE){
+        } else if (Player.getGrade() == Grade.SOPHOMORE) {
             map = importTXT("Banner/map-" + GameClient.getPlayer().getGrade().toString() + ".txt");
             mapCode.replace(GameClient.getPlayer().getLocation(), "**");
             newMap = String.format(map, mapCode.get("Gym"), mapCode.get("Geography"),
-                    mapCode.get("Player"), mapCode.get("History"),mapCode.get("Hallway"),
-                    mapCode.get("Credit"), mapCode.get("Cafeteria"), mapCode.get("Maths"),
-                    mapCode.get("Computers"));
-        }
-        else if (Player.getGrade() == Grade.JUNIOR){
+                    mapCode.get("Player"), mapCode.get("History"), mapCode.get("Hallway"),
+                    mapCode.get("Grade"), mapCode.get("Cafeteria"), mapCode.get("Maths"),
+                    mapCode.get("Credit"), mapCode.get("Computers"));
+        } else if (Player.getGrade() == Grade.JUNIOR) {
             map = importTXT("Banner/map-" + GameClient.getPlayer().getGrade().toString() + ".txt");
             mapCode.replace(GameClient.getPlayer().getLocation(), "**");
             newMap = String.format(map, mapCode.get("Computers"), mapCode.get("Player"),
-                    mapCode.get("Hallway"),mapCode.get("Gym"), mapCode.get("Credit"),
-                    mapCode.get("Cafeteria"), mapCode.get("Maths"), mapCode.get("Geography"),
-                    mapCode.get("History"));
-        }
-        else if (Player.getGrade() == Grade.SENIOR){
+                    mapCode.get("Hallway"), mapCode.get("Gym"), mapCode.get("Grade"),
+                    mapCode.get("Cafeteria"), mapCode.get("Credit"), mapCode.get("Maths"),
+                    mapCode.get("Geography"), mapCode.get("History"));
+        } else if (Player.getGrade() == Grade.SENIOR) {
             map = importTXT("Banner/map-" + GameClient.getPlayer().getGrade().toString() + ".txt");
             mapCode.replace(GameClient.getPlayer().getLocation(), "**");
             newMap = String.format(map, mapCode.get("Computers"), mapCode.get("Player"),
-                    mapCode.get("History"),mapCode.get("Gym"), mapCode.get("Credit"),
-                    mapCode.get("Hallway"), mapCode.get("Geography"), mapCode.get("Cafeteria"),
-                    mapCode.get("Maths"));
+                    mapCode.get("History"), mapCode.get("Gym"), mapCode.get("Grade"),
+                    mapCode.get("Hallway"), mapCode.get("Geography"), mapCode.get("Credit"),
+                    mapCode.get("Cafeteria"), mapCode.get("Maths"));
         }
         return newMap;
     }
-
 
     public static String importTXT(String path) {
         String result = null;
