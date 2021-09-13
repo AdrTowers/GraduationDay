fe0e0630460f911c39e2cb9a64271fa05cf0dbb5
AdrianT <74223068+AdrTowers@users.noreply.github.com>
2021-09-13 09:50:29 -0700

    Stubbing out volume increase method. AB#344

--- src/main/java/com/graduation/utils/SoundEffects.java
+++ src/main/java/com/graduation/utils/SoundEffects.java
@@ -8,6 +8,12 @@ import java.util.Scanner;
 
 public class SoundEffects {
 
+    /**
+     * Plays 'positive' sound when user gets a correct answer
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws LineUnavailableException
+     */
     public static void correctAnswer() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 
         Scanner scanner = new Scanner(System.in);
@@ -15,10 +21,17 @@ public class SoundEffects {
         AudioInputStream audioInput = AudioSystem.getAudioInputStream(file);
         Clip clip = AudioSystem.getClip();
         clip.open(audioInput);
-
+//        FloatControl decrease = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+//        decrease.setValue(-10);
         clip.start();
     }
 
+    /**
+     * Plays 'negative' sound when user gets an incorrect answer
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws LineUnavailableException
+     */
     public static void incorrectAnswer() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         Scanner scanner = new Scanner(System.in);
         File file = new File("Audio/fail.wav");
@@ -29,13 +42,24 @@ public class SoundEffects {
         clip.start();
     }
 
+    /**
+     * Plays a 'congratulatory' sound when user passes into the next grade
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws LineUnavailableException
+     */
     public static void movingToNextGrade() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         Scanner scanner = new Scanner(System.in);
         File file = new File("Audio/Levelup.wav");
         AudioInputStream audioInput = AudioSystem.getAudioInputStream(file);
         Clip clip = AudioSystem.getClip();
         clip.open(audioInput);
-
+        //increaseVolume(clip, vol);
         clip.start();
+    }
+
+    public static void increaseVolume(Clip clip, Double vol) throws LineUnavailableException {
+        FloatControl decrease = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        decrease.setValue(-10);
     }
 }
